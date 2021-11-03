6a686b8d4cec1a358636f369b6f3ba4f71190700
Nathalia Vieira <naahviera@gmail.com>
2021-11-03 12:16:00 -0300

    mesa aula 7

--- src/container/App.js
+++ src/container/App.js
@@ -3,19 +3,46 @@ import '../styles/App.css';
 import ClassComponent from '../componentes/ClassComponent';
 import FunctionComponent from '../componentes/FunctionComponent';
 
+
+
 function App() {
+  const listaConvidados = [
+   {
+    nome : 'Nicolas',
+    estaNaLista : true,
+    tarefa : 'batata-frita'
+  },
+  {
+    nome : 'Pedro',
+    estaNaLista : false,
+    tarefa : 'pizza'
+  },
+  {
+    nome : 'Carolina',
+    estaNaLista : true,
+    tarefa : 'bebidas'
+  }  
+  ]
+ 
   
   return (
     <div className="App">
     <h3>Convidado:</h3>
-    <ClassComponent nome="Nicolas" estaNaLista={true} />
-    <ClassComponent nome="Pedro" estaNaLista={false} />
-    <ClassComponent nome="Carolina" estaNaLista={true} />
+    {listaConvidados.map((convidados,index)=> {
+      return(
+        <>
+       <ClassComponent key = {index} nome={convidados.nome} estaNaLista={convidados.estaNaLista} /> 
+       </>
+    )})}
+
     <h3>Tarefas: </h3>
-    <FunctionComponent nome="Nicolas" tarefa="batata-frita" />
-    <FunctionComponent nome="Pedro" tarefa="pizza" />
-    <FunctionComponent nome="Carolina" tarefa="bebidas" />
-  </div>
+    {listaConvidados.map((convidados,index)=>{
+      return(
+        <>
+        <FunctionComponent key = {index} nome={convidados.nome} tarefa={convidados.tarefa} />
+        </>
+      )})}
+    </div>
   );
 }
 
